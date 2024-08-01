2382f93756c0276242f4a8aea6e3b79c94cd4bc9
Akramelbahar <113865830+Akramelbahar@users.noreply.github.com>
2024-08-01 21:27:53 +0000

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -24,12 +24,12 @@ function Navbar({btnSignup ,isChat,isDashboard, btnLogin , btnLogout , profile})
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <div className={profile=="none "? "hidden": "md:hidden flex self-stretch items-wrap "  }  ><a href="/profile"><Profile></Profile></a></div>
+          <div className={profile=="none "? "hidden": "md:hidden flex  "  }  ><a href="/profile"><Profile></Profile></a></div>
   
-        <a href="/dashboard"><li className={(btnLogout == "none" ) || isDashboard ? "hidden": "btn ring my-2  btn-accent ml-1 mr-1"  }  >Dashboard</li> </a>
-        <a href="/login"><li className={btnLogin == "none" ? "hidden" : "btn ring my-2   btn-accent ml-1 mr-1"}>Se Connecter</li></a>
-        <a href="/chat"><li className={btnLogout == "none" || isChat  ? "hidden" : "btn ring my-2  btn-accent ml-1 mr-1"}>💬</li></a>
-        <a href="signup"><li className={btnSignup=="none" ? "hidden": "btn ring my-2  btn-accent ml-1 mr-1"  }  >S'Inscrire</li></a>
+        <a  className="btn  md:hidden btn-accent ml-1 ring my-2 mr-1" href="/dashboard"><li className={(btnLogout == "none" ) || isDashboard ? "hidden": "btn ring my-2  btn-accent ml-1 mr-1"  }  >Dashboard</li> </a>
+        <a  className="btn  md:hidden btn-accent ml-1 ring my-2 mr-1" href="/login"><li className={btnLogin == "none" ? "hidden" : "btn ring my-2   btn-accent ml-1 mr-1"}>Se Connecter</li></a>
+        <a  className="btn  md:hidden btn-accent ml-1 ring my-2 mr-1" href="/chat"><li className={btnLogout == "none" || isChat  ? "hidden" : "btn ring my-2  btn-accent ml-1 mr-1"}>💬</li></a>
+        <a  className="btn  md:hidden btn-accent ml-1 ring my-2 mr-1" href="signup"><li className={btnSignup=="none" ? "hidden": "btn ring my-2  btn-accent ml-1 mr-1"  }  >S'Inscrire</li></a>
         <a className="btn  md:hidden btn-accent ml-1 ring my-2 mr-1" href="insertAd">Publier Une Annonce</a>
         
         <a href="/logout"><li className={btnLogout == "none"? "hidden": "btn ring my-2  btn-accent ml-1 mr-1"  }  >Se Deconnecter</li></a>

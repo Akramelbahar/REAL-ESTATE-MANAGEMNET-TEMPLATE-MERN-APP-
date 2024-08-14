bc34afac9a5f5665bb6fa9cc01b820a58926eb98
Akramelbahar <113865830+Akramelbahar@users.noreply.github.com>
2024-08-14 12:07:03 +0000

    Update ads.routest.js

--- backend/routes/ads.routest.js
+++ backend/routes/ads.routest.js
@@ -1,10 +1,11 @@
 import express from "express";
 import protectRoute from "../utils/middleware/protectroute.js";
-import { getAdById, getAds, searchAds } from "../controllers/ads.controller.js"
+import { getAdById, getAds, totalPages , searchAds } from "../controllers/ads.controller.js"
 
 const router = express.Router();
 
 router.get("", getAds);
 router.get("/search" ,searchAds)
 router.get("/:advertismentId" ,getAdById);
+router.get("/pageCount" , totalPages);
 export default router;

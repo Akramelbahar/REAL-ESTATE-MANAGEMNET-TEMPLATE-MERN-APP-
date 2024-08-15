8f31415d07eb468ddc004d0be90082b085c4f1e3
Akramelbahar <113865830+Akramelbahar@users.noreply.github.com>
2024-08-15 00:38:52 +0000

    Update AdDetails.jsx

--- frontend/src/pages/AdDetails.jsx
+++ frontend/src/pages/AdDetails.jsx
@@ -1,173 +1,212 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import { useAuth } from '../../context/AuthContext';
-import axios from 'axios';
 import Footer from '../components/Footer';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+import ToggleTheme from '../components/ToggleTheme';
+import Carousel from '../components/Carousselle';
+import axios from 'axios';
+import { UserRole } from '../components/UserRole';
 import toast, { Toaster } from 'react-hot-toast';
-const timeSince = (date) => {
-  const seconds = Math.floor((new Date() - new Date(date)) / 1000);
 
-  const days = Math.floor(seconds / 86400);
-  const hours = Math.floor((seconds % 86400) / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  const remainingSeconds = seconds % 60;
-
-  return `${days} days, ${hours} hours, ${minutes} minutes, and ${remainingSeconds} seconds ago`;
-};
-
-function AdDetails() {
-  const { id } = useParams();
-  const [userId , setUserId] = useState();
-  const { authToken } = useAuth();
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [creatorName, setCreatorName] = useState("");
-  const [creatorPicture, setCreatorPicture] = useState("");
-  const [creatorUsername, setCreatorUsername] = useState("");
-  const [price, setPrice] = useState("");
-  const [surface, setSurface] = useState("");
-  const [pcs, setPcs] = useState("");
-  const [typeBien, setTypeBien] = useState("");
-  const [adresse, setAdresse] = useState("");
-  const [pictures, setPictures] = useState([]);
-  const [diagnostic, setDiagnostic] = useState("");
-  const [equipment, setEquipment] = useState("");
-  const [createdAt, setCreatedAt] = useState("");
-  const [tel, setTel] = useState("");
-  const [addFav, setAddFav] = useState(false);
-  
+function InsertAd() {
   const navigate = useNavigate();
+  const { authToken } = useAuth();
 
   useEffect(() => {
-    axios.get(`https://backend-hgsc.onrender.com/api/listing/${id}` , {headers : {
-      token : authToken
-    }})
-      .then((response) => {
-        const ad = response.data.data;
-        setUserId(ad.createdBy._id)
-        setTitle(ad.title);
-        setDescription(ad.description);
-        setPrice(ad.price);
-        setSurface(ad.surface);
-        setPcs(ad.pcs);
-        setTypeBien(ad.type);
-        setAdresse(ad.adresse);
-        setPictures(ad.pictures);
-        setDiagnostic(ad.diagnostic);
-        setEquipment(ad.equipment);
-        setCreatedAt(ad.createdAt);
-        setCreatorName(ad.createdBy.FirstName + " " + ad.createdBy.LastName);
-        setCreatorPicture(ad.createdBy.profile_pic);
-        setCreatorUsername(ad.createdBy.username);
-        setTel(ad.createdBy.tel);
-      })
-      .catch((error) => {
-        console.error(error.response.data);
-      });
-  }, [id]);
-
-  const timeSinceAdCreated = createdAt ? timeSince(createdAt) : "";
-  
-  const sendMessage = () => {
-    if (!authToken) navigate("/login");
-    else {
-        navigate("/chat/"+userId);
-    }
-  };
-
-  const addToFav = async () => {
     if (!authToken) {
       navigate("/login");
-    } else {
-      try {
-        const response = await axios.get(`https://backend-hgsc.onrender.com/api/user/advertisment/favorite/${id}`, {
-          headers: {
-            token:authToken
-          }
-        });
-        setAddFav(true);
-        toast.success("Ajouté a vos favoris")
-      } catch (error) {
-        console.error("Error adding to favorites:", error);
-        toast.error("Error adding to favorites");
+    }
+  }, [authToken, navigate]);
+
+  const [Title, setTitle] = useState("");
+  const [Adresse, setAdresse] = useState("");
+  const [Description, setDescription] = useState("");
+  const [Prix, setPrix] = useState(0);
+  const [Pcs, setPcs] = useState(0);
+  const [Surface, setSurface] = useState(0);
+  const [TypeBien, setTypeBien] = useState("");
+  const [Diagnostics, setDiagnostics] = useState("");
+  const [Equipements, setEquipements] = useState("");
+  const [Pictures, setPictures] = useState([]);
+  const [Html, setHtml] = useState(<Carousel items={[]} />);
+  const [Post, setPost] = useState(false);
+
+  // Validation errors
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    if (Post) {
+      navigate("/dashboard");
+    }
+  }, [Post, navigate]);
+
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (!Title) newErrors.Title = "Le titre est requis.";
+    if (!Adresse) newErrors.Adresse = "L'adresse est requise.";
+    if (!Description) newErrors.Description = "La description est requise.";
+    if (Prix <= 0) newErrors.Prix = "Le prix doit être un nombre positif.";
+    if (Pcs <= 0) newErrors.Pcs = "Le nombre de pièces doit être un nombre positif.";
+    if (Surface <= 0) newErrors.Surface = "La surface doit être un nombre positif.";
+    if (!TypeBien) newErrors.TypeBien = "Le type de bien est requis.";
+
+    setErrors(newErrors);
+
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const publish = async () => {
+    if (!validateForm()) {
+      toast.error("Veuillez corriger les erreurs dans le formulaire.");
+      return;
+    }
+
+    try {
+      const response = await axios.post('http://backend-hgsc.onrender.com/api/user/advertisment', {
+        title: Title,
+        description: Description,
+        price: Prix,
+        surface: Surface,
+        pcs: Pcs,
+        type: TypeBien,
+        adresse: Adresse,
+        pictures: Pictures,
+        diagnostic: Diagnostics,
+        equipment: Equipements
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'token': authToken,
+        },
+        withCredentials: true,
+      });
+      if (response.status === 201) {
+        toast.success("Votre advertisement a été publié");
+        setTimeout(() => {
+          setPost(true);
+        }, 150);
+      } else {
+        toast.error("Erreur en validation de vos infos.");
       }
+    } catch (error) {
+      toast.error("Erreur en validation de vos infos.");
     }
   };
+
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      try {
+        const role = await UserRole(authToken);
+        if (role === "user") {
+          navigate("/UserError");
+        }
+      } catch (error) {
+        console.error('Error fetching user role:', error);
+      }
+    };
+
+    fetchUserRole();
+  }, [authToken, navigate]);
+
+  const handleFileChange = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await axios.post('http://localhost:5000/upload', formData);
+      const fileUrl = response.data.url;
+      setPictures((prevPictures) => [...prevPictures, fileUrl]);
+      e.target.value = null;
+    } catch (err) {
+      console.error('Error uploading file:', err);
+    }
+  };
+
+  const removePicture = () => {
+    const hash = window.location.hash.substring(1);
+    const itemIndex = hash.startsWith("item") ? parseInt(hash.substring(4)) : -1;
+
+    if (itemIndex >= 0 && itemIndex < Pictures.length) {
+      setPictures((prevPictures) => prevPictures.filter((_, index) => index !== itemIndex));
+    }
+  };
+
+  useEffect(() => {
+    setHtml(<Carousel items={Pictures} />);
+  }, [Pictures]);
 
   return (
     <>
-      <Navbar
-        btnSignup="none"
-        btnLogin="none"
-        MonCompte="display"
-        btnLogout={authToken ? "" : "none"}
-        profile={authToken ? "" : "none"}
-      />
-      <div className='bg-base-300 h-[80vh] lg:mx-[15vh] my-5 rounded overflow-x-auto'>
-        <p className='mx-3 my-4 sm:mx-[10vh] block lg:my-6 font-semibold text-gray-500'>Crée il y'a : {timeSinceAdCreated}</p>
-        <Caroussel items={pictures} />
-        <div className='p-3 flex flex-col items-center lg:flex-row-reverse gap-5 lg:gap-3'>
-          <div className='mt-2 w-fit bg-base-100 px-12 rounded shadow border border-white lg:border-0 py-4 lg:m-0 lg:w-1/4 overflow-y-auto lg:bg-inherit'>
-            <img src={creatorPicture} alt={`${creatorName}'s profile`} className='w-12 h-12 rounded-full mr-4' />
-            <div>
-              <h2 className='text-xl font-bold'>Createur:</h2>
-              <p className='text-lg font-semibold'>{creatorName}</p>
-              <p className='text-sm text-gray-400 font-semibold'>{tel}</p>
-              <p className='text-sm text-gray-600'>@{creatorUsername}</p>
-              <button className="btn block btn-info my-4 w-fit mx-auto"  onClick={sendMessage}>Envoyez Message au vendeur</button>
-              <button className="btn block btn-warning my-4 mx-auto w-fit" disabled={addFav} onClick={addToFav}>Ajouter Au Favoris</button>
-            </div>
+      <ToggleTheme />
+      <Navbar btnLogin={"none"} btnSignup={"none"} />
+      <div className='relative xl:mx-12 xl:my-4 lg:mx-8 lg:my-4 md:mx-6 md:my-3 sm:mx-8 sm:my-2 my-2 mx-4 min-h-screen rounded-md shadow-xl ring p-4 bg-base-300'>
+        <div role="alert" className="alert m-3 space-y-2 block">
+          <h3 className='block text-xl text-bold font-bold'>Qu'annoncez-vous aujourd'hui ?</h3>
+          <span className='block'>Grâce à ces informations les acheteurs peuvent trouver votre annonce plus facilement</span>
+        </div>
+        <div className='md:w-3/5 w-full mx-auto'>
+          <select required defaultValue="0" onChange={(e) => setTypeBien(e.target.value)} className="input p-3 my-3 block w-full input-bordered">
+            <option disabled value="0">Type de bien</option>
+            <option value="achat">Achat</option>
+            <option value="vente">Vente</option>
+            <option value="location">Location</option>
+            <option value="Autre">Autre</option>
+          </select>
+          {errors.TypeBien && <p className="text-red-500">{errors.TypeBien}</p>}
+        
+          <input type="text" placeholder="Titre De L'Annonce" className="input p-3 my-3 block w-full input-bordered" onChange={(e) => setTitle(e.target.value)} />
+          {errors.Title && <p className="text-red-500">{errors.Title}</p>}
+
+          <input type="text" placeholder="Adresse du bien" className="input p-3 my-3 block w-full input-bordered" onChange={(e) => setAdresse(e.target.value)} />
+          {errors.Adresse && <p className="text-red-500">{errors.Adresse}</p>}
+        </div>
+
+        <div className='flex flex-col content-center justify-center  gap-2 w-full flex-wrap'>
+          <textarea className="textarea textarea-bordered w-full mx-auto md:w-3/5" onChange={(e) => setDescription(e.target.value)} placeholder="Description"></textarea>
+          {errors.Description && <p className="text-red-500">{errors.Description}</p>}
+        </div>
+
+        <div className='m-3 flex content-center gap-3 justify-center flex-wrap lg:flex-nowrap'>
+          <div className='flex flex-col w-full md:w-1/6'>
+          <input type="number" placeholder="Nombre de pièces" min={0} onChange={(e) => setPcs(e.target.value)} className="input" />
+          {errors.Pcs && <p className="text-red-500">{errors.Pcs}</p>}
           </div>
-          <div className='w-fit md:w-10/12 lg:w-3/4'>
-            <h1 className='text-2xl underline font-extrabold overflow-auto'>{title}</h1>
-            <p className='text-xl font-extrabold overflow-auto ml-5 m-4'>Adresse: {adresse}</p>
-            <h3 className='text-xl font-extrabold overflow-auto ml-5 m-4'>Description :</h3>
-            <p className='text-lg w-11/12 my-4 rounded mx-auto bg-base-200 shadow-xl p-2'>{description}</p>
-            <p className='text-xl font-extrabold overflow-auto ml-5 m-4'>Prix: {price}</p>
-            <p className='text-xl font-extrabold overflow-auto ml-5 m-4'>Surface: {surface}</p>
-            <p className='text-xl font-extrabold overflow-auto ml-5 m-4'>Pcs: {pcs}</p>
-            <p className='text-xl font-extrabold overflow-auto ml-5 m-4'>Type: {typeBien}</p>
-            <p className='text-xl font-extrabold overflow-auto ml-5 m-4'>Diagnostic: {diagnostic}</p>
-            <p className='text-xl font-extrabold overflow-auto ml-5 m-4'>Equipment: {equipment}</p>
+          <div className='flex flex-col w-full md:w-1/6'>
+          <input type="number" placeholder="Surface" min={0} onChange={(e) => setSurface(e.target.value)} className="input " />
+          {errors.Surface && <p className="text-red-500">{errors.Surface}</p>}
+          </div>
+          <div className='flex flex-col w-full md:w-1/6'>
+          <input type="number" placeholder="Prix" min={0} onChange={(e) => setPrix(e.target.value)} className="input" />
+          {errors.Prix && <p className="text-red-500">{errors.Prix}</p>}
           </div>
         </div>
+
+        <div className='flex content-center justify-center md:flex-nowrap flex-wrap'>
+          <textarea className="textarea textarea-ghost my-3 md:w-2/6 w-5/6" onChange={(e) => setDiagnostics(e.target.value)} placeholder="Diagnostics"></textarea>
+          <textarea className="textarea textarea-ghost my-3 md:w-2/6 w-5/6" onChange={(e) => setEquipements(e.target.value)} placeholder="Équipements"></textarea>
+        </div>
+
+        <div className="flex w-full flex-col lg:flex-row items-center flex-wrap my-4">
+          <div className={"card bg-base-100 rounded-box flex flex-grow place-items-center h-96 justify-center w-3/4 relative" + (Pictures.length === 0 ? " after:content-[attr(after)] lg:after:content-[attr(aftermd)] " : "")} after="Inserer une image du dessous" aftermd="Inserer une image du droite">
+            <span className='block absolute top-10 right-10 text-3xl hover:text-4xl hover:bg-base-300 p-2 ease-in duration-100 hover:rounded-full hover:p-4' onClick={removePicture}>❌</span>
+            {Html}
+          </div>
+          <div className="divider lg:divider-horizontal"></div>
+          <div className="card bg-base-100 rounded-box grid h-20 flex-grow place-items-center">
+            <button className="btn btn-active btn-ghost ring-2" onClick={() => document.getElementById("file-upload").click()}>Ajouter une photo</button>
+            <input accept="image/*" type="file" id="file-upload" className='hidden' onChange={handleFileChange}></input>
+          </div>
+        </div>
+
+        <button className="btn md:w-1/4 mx-3 btn-accent w-4/10 block ms-auto" onClick={publish}>Publier l'annonce</button>
       </div>
       <Footer />
-      <Toaster/>
+      <Toaster />
     </>
   );
 }
 
-function Caroussel({ items }) {
-  return (
-    <div className="carousel p-2 m-2md:m-12 w-fit rounded h-[50vh]">
-      {items.map((e, index) => (
-        <div
-          id={"slide" + (1 + index).toString()}
-          className="carousel-item relative w-full"
-          key={index}
-        >
-          <img src={e} className="w-full" />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a
-              href={"#slide" + (index === 0 ? items.length : index)}
-              className="btn btn-circle"
-            >
-              ❮
-            </a>
-            <a
-              href={"#slide" + (index === items.length - 1 ? 1 : index + 2)}
-              className="btn btn-circle"
-            >
-              ❯
-            </a>
-          </div>
-        </div>
-      ))}
-      
-    </div>
-  );
-}
-
-export default AdDetails;
+export default InsertAd;
